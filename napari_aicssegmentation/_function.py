599158ce9ac749afa526142f36e9f8885ec5fadc
heeler <jamie.sherman@gmail.com>
2021-02-15 21:57:25 -0800

    fixed formatting

--- napari_aicssegmentation/_function.py
+++ napari_aicssegmentation/_function.py
@@ -26,15 +26,18 @@ def napari_experimental_provide_function():
     return [threshold, image_arithmetic]
 
 
-# 1.  First example, a simple function that thresholds an image and creates a labels layer
+# 1.  First example, a simple function that thresholds an image
+# and creates a labels layer
 def threshold(data: "ImageData", threshold: int) -> "LabelsData":
     """Threshold an image and return a mask."""
     return (data > threshold).astype(int)
 
 
-# 2. Second example, a function that adds, subtracts, multiplies, or divides two layers
+# 2. Second example, a function that adds, subtracts,
+# multiplies, or divides two layers
 
-# using Enums is a good way to get a dropdown menu.  Used here to select from np functions
+# using Enums is a good way to get a dropdown menu.
+# Used here to select from np functions
 class Operation(Enum):
     add = np.add
     subtract = np.subtract
