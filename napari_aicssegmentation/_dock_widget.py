c5508d7e58e184450983365b62ba0e6418ad27ac
Na Hyung Choi <schoinh@gmail.com>
2021-03-11 18:39:52 -0800

    Implement title and step 1

--- napari_aicssegmentation/_dock_widget.py
+++ napari_aicssegmentation/_dock_widget.py
@@ -4,7 +4,9 @@ import napari
 from napari_aicssegmentation.core.application import Application
 from napari_aicssegmentation.util.debug_utils import debug_class
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QVBoxLayout
+from qtpy.QtWidgets import QComboBox, QLabel, QMessageBox, QPushButton, QTextEdit, QVBoxLayout, QWidget
+
+from ._stylesheet import GLOBAL_STYLESHEET
 
 """
 The class name here gets converted to title case and gets displayed as both the title 
@@ -16,10 +18,57 @@ of the plugin window and the title displayed in the app menu dropdown.
 class AllenCellStructureSegmenter(QWidget):  # pragma: no-cover
     def __init__(self, napari_viewer: napari.Viewer):
         super().__init__()
+        self.viewer = napari_viewer
+        self.setStyleSheet(GLOBAL_STYLESHEET)
+
+        title = QLabel("Segmentation workflow selection")
+        title.setWordWrap(True)
+        title.setStyleSheet("QLabel { font-weight: bold; font-size: 20px; margin-bottom: 1em }")
+
+        # Need to supply HTML because of this bug: https://bugreports.qt.io/browse/QTBUG-90853
+        step_1 = QLabel("<span>1.&nbsp;Select a channel to segment:</span>")
+        dropdown = QComboBox()
+        dropdown.addItem("Channel 1")
+        dropdown.addItem("Channel 2")
+        dropdown.addItem("Channel 3")
 
         self.setLayout(QVBoxLayout())
-        self._application = Application(napari_viewer, self.layout())
-        self._application.router.mpp()  # Initialize first screen
+        self.layout().addWidget(title)
+        self.layout().addWidget(step_1)
+        self.layout().addWidget(dropdown)
+        self.layout().addStretch()
+    
+    # def smooth_image(self):
+    #     """Guassian Blur on an image, and add it as a new layer"""
+    #     if self.has_image():
+    #         # Name to add based off previous image's name
+    #         name = (
+    #             self.viewer.layers[len(self.viewer.layers) - 1].name + ": Guassian Blur"
+    #         )
+
+    #         # Adding the image to the viewer
+    #         self.viewer.add_image(
+    #             image_smoothing_gaussian_3d(self.viewer.layers[0].data, sigma=3.0),
+    #             name=name,
+    #         )
+    #     else:
+    #         self.show_message_box(
+    #             "Error: No Image", "Load an image before running guassian blur"
+    #         )
+
+    # def has_image(self):
+    #     """Determines if there is already an image loaded onto napari"""
+    #     if len(self.viewer.layers) == 0:
+    #         return False
+    #     else:
+    #         return True
+
+    # def show_message_box(self, title, message):
+    #     """Show a message box with the specified title and message"""
+    #     msg = QMessageBox()
+    #     msg.setWindowTitle(title)
+    #     msg.setText(message)
+    #     return msg.exec()
 
 
 @napari_hook_implementation
---
+++ napari_aicssegmentation/_stylesheet.py
@@ -0,0 +1,14 @@
+"""
+Global stylesheet
+
+Qt stylesheet reference: https://doc.qt.io/qtforpython-5/overviews/stylesheet.html
+Examples: https://doc.qt.io/qt-5/stylesheet-examples.html#customizing-specific-widgets
+"""
+
+GLOBAL_STYLESHEET = (
+    """
+    QLabel {
+        font-size: 16px;
+    }
+    """
+)
