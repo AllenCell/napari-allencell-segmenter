6aa39f7a9dadda3d06c312542dd7716d71d7577a
Na Hyung Choi <schoinh@gmail.com>
2021-03-23 18:39:28 -0700

    Add doc blocks for reusable functions

--- napari_aicssegmentation/_dock_widget.py
+++ napari_aicssegmentation/_dock_widget.py
@@ -96,7 +96,15 @@ class AllenCellStructureSegmenter(QWidget):
         self.add_workflow_buttons()
         self.page.layout().addStretch()
 
-    """ Return a QWidget containing a warning icon and a message """
+    # TODO: move this to general utils
+    """ 
+    Return a QWidget containing a warning icon and a message.
+
+    Inputs:
+        message:    String
+    Output:
+        A QWidget displaying a warning symbol and a message next to it
+    """
     def warning_message(self, message, should_display=False):
         if should_display == False:
             return None
@@ -115,12 +123,28 @@ class AllenCellStructureSegmenter(QWidget):
         widget.layout().addStretch()
         return widget
     
+    # TODO: move this to general utils
+    """
+    Create a nicely formatted form layout given contents to add as rows.
+
+    Inputs:
+        rows:       List of dictionaries with this shape:
+                        {
+                            "label": string,
+                            "input": QWidget (e.g., QLabel, QComboBox)
+                        }
+        margins:    Tuple of 4 numbers representing left, top, right, and bottom margins for
+                    the form's contents
+    Output:
+        A QFrame widget with a QFormLayout
+    """
     def create_form(self, rows, margins=(0, 5, 11, 0)):
         widget = QFrame()
         layout = QFormLayout()
         layout.setFormAlignment(Qt.AlignLeft)
         left, top, right, bottom = margins
         layout.setContentsMargins(left, top, right, bottom)
+
         for row in rows:
             layout.addRow(row["label"], row["input"])
         widget.setLayout(layout)
