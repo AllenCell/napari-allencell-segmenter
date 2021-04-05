fde1bb82785671ec430bf37ab8475b5ddf39b584
Na Hyung Choi <schoinh@gmail.com>
2021-04-05 11:45:59 -0700

    Add workflow step title

--- napari_aicssegmentation/view/workflow_steps_view.py
+++ napari_aicssegmentation/view/workflow_steps_view.py
@@ -1,4 +1,11 @@
-from qtpy.QtWidgets import QLabel, QPushButton, QVBoxLayout
+from qtpy.QtWidgets import (
+    QHBoxLayout,
+    QLabel, 
+    QLayout, 
+    QPushButton, 
+    QVBoxLayout, 
+    QWidget
+)
 from napari_aicssegmentation.model.segmenter_model import SegmenterModel
 from napari_aicssegmentation.util.debug_utils import debug_class
 from napari_aicssegmentation.controller._interfaces import IWorkflowStepsController
@@ -8,7 +15,7 @@ from ._main_template import MainTemplate
 
 @debug_class
 class WorkflowStepsView(View):  # pragma: no-cover
-    _lbl_selected_workflow: QLabel
+    # _lbl_selected_workflow: QLabel
 
     def __init__(self, controller: IWorkflowStepsController):
         super().__init__(template_class=MainTemplate)
@@ -19,21 +26,43 @@ class WorkflowStepsView(View):  # pragma: no-cover
 
     def setup_ui(self):
         layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
         self.setLayout(layout)
 
-        lbl_title = QLabel("Workflow steps")
-        self._lbl_selected_workflow = QLabel()
+        # lbl_title = QLabel("Workflow steps")
+        # self._lbl_selected_workflow = QLabel()
 
-        btn_back = QPushButton("Back")
-        btn_back.clicked.connect(self._btn_back_clicked)
+        self._add_workflow_title(layout)
 
-        layout.addWidget(lbl_title)
-        layout.addWidget(self._lbl_selected_workflow)
-        layout.addWidget(btn_back)
+        # btn_back = QPushButton("Back")
+        # btn_back.clicked.connect(self._btn_back_clicked)
+
+        # layout.addWidget(lbl_title)
+        # layout.addWidget(self._lbl_selected_workflow)
+        # layout.addWidget(btn_back)
 
     def load_model(self, model: SegmenterModel):
-        self._lbl_selected_workflow.setText(f"Selected workflow: {model.active_workflow}")
-        self._lbl_selected_workflow.repaint()
+        pass
+        # self._lbl_selected_workflow.setText(f"Selected workflow: {model.active_workflow}")
+        # self._lbl_selected_workflow.repaint()
 
-    def _btn_back_clicked(self, checked: bool):
-        self._controller.navigate_back()
+    # def _btn_back_clicked(self, checked: bool):
+    #     self._controller.navigate_back()
+
+    def _add_workflow_title(self, layout: QLayout):
+        widget = QWidget()
+        title_layout = QHBoxLayout()
+        widget.setLayout(title_layout)
+
+        config_workflow_name = "sec61b"
+        workflow_name = QLabel(f"Workflow: {config_workflow_name}")
+        info = QLabel("ⓘ")
+
+        title_layout.addStretch()
+        title_layout.addWidget(workflow_name)
+        title_layout.addWidget(info)
+        title_layout.addStretch()
+
+        title_layout.setSpacing(0)
+        widget.setObjectName("workflowTitle")
+        layout.addWidget(widget)
