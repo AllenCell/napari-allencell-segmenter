ea28ed74d3ed408ddcd5811eb0f370fe4b81f72c
Na Hyung Choi <schoinh@gmail.com>
2021-04-09 13:02:46 -0700

    Add blank tests

--- napari_aicssegmentation/_tests/view/workflow_steps_view_test.py
+++ napari_aicssegmentation/_tests/view/workflow_steps_view_test.py
@@ -4,7 +4,6 @@ from unittest.mock import MagicMock, create_autospec
 from napari_aicssegmentation.view.workflow_steps_view import (
     WorkflowStepsView,
     IWorkflowStepsController,
-    SegmenterModel,
 )
 
 
@@ -13,3 +12,13 @@ class TestWorkflowSelectView:
         self._mock_controller: MagicMock = create_autospec(IWorkflowStepsController)
         self._view = WorkflowStepsView(self._mock_controller)
         self._view.setup_ui()
+
+    def test_add_workflow_title(self):
+        pass
+
+    def test_add_progress_bar(self):
+        pass
+
+    def test_add_workflow_steps(self):
+        pass
+
