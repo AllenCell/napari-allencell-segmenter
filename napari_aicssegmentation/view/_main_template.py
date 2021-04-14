9fdeb55041d4d4e539e3c140f271ecf4a5cf2f6f
Na Hyung Choi <schoinh@gmail.com>
2021-04-13 18:00:22 -0700

    Make new test workflow step

--- napari_aicssegmentation/view/_main_template.py
+++ napari_aicssegmentation/view/_main_template.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QFrame, QVBoxLayout, QScrollArea, QLabel
 from PyQt5 import QtCore
 from napari_aicssegmentation._style import Style
 
-from napari_aicssegmentation.widgets.workflow_step_box import WorkflowStepBox
+from napari_aicssegmentation.widgets.workflow_step_widget import WorkflowStepWidget
 # from aicssegmentation.structure_wrapper.WorkflowStep import WorkflowStep
 
 
@@ -57,18 +57,52 @@ class MainTemplate(ViewTemplate):
 
         test_workflow_step = dict()
         test_workflow_step["display_name"] = "intensity_normalization"
-        test_workflow_step["function"] = {"parameters": [3, 15]}
+        test_workflow_step["function"] = {
+            "display_name": "Step 1",
+            "function": "Function 1",
+            "module": "Module 1",
+            "parameters": {
+                "scaling_param": [
+                    {
+                        "widget_type": "slider",
+                        "data_type": int,
+                        "min_value": 1,
+                        "max_value": 5,
+                        "default_value": 2,
+                    },
+                    {
+                        "widget_type": "slider",
+                        "data_type": float,
+                        "min_value": 0.5,
+                        "max_value": 3.5,
+                        "default_value": 1.1,
+                    },
+                ],
+                "other_param": [
+                    {
+                        "widget_type": "drop-down",
+                        "data_type": bool,
+                        "min_value": 0.5,
+                        "option": [
+                            "True",
+                            "False"
+                        ]
+                    },
+                ]
+            },
+        }
+
         # test_workflow_step = WorkflowStep(test_workflow_step)
 
-        page.layout().addWidget(WorkflowStepBox(test_workflow_step))  # Test code
+        page.layout().addWidget(WorkflowStepWidget(test_workflow_step))  # Test code
 
-        test_workflow_step = dict()
-        test_workflow_step["display_name"] = "intensity_normalization_with_bound"
-        test_workflow_step["function"] = {"parameters": [3, 15]}
-        # test_workflow_step = WorkflowStep(test_workflow_step)
-        page.layout().addWidget(WorkflowStepBox(test_workflow_step))
-        test_workflow_step = dict()
-        test_workflow_step["display_name"] = "size_filter"
-        test_workflow_step["function"] = {"parameters": [1, 2]}
-        # test_workflow_step = WorkflowStep(test_workflow_step)
-        page.layout().addWidget(WorkflowStepBox(test_workflow_step))
+        # test_workflow_step = dict()
+        # test_workflow_step["display_name"] = "intensity_normalization_with_bound"
+        # test_workflow_step["function"] = {"parameters": [3, 15]}
+        # # test_workflow_step = WorkflowStep(test_workflow_step)
+        # page.layout().addWidget(WorkflowStepWidget(test_workflow_step))
+        # test_workflow_step = dict()
+        # test_workflow_step["display_name"] = "size_filter"
+        # test_workflow_step["function"] = {"parameters": [1, 2]}
+        # # test_workflow_step = WorkflowStep(test_workflow_step)
+        # page.layout().addWidget(WorkflowStepWidget(test_workflow_step))
