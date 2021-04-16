7f1ce1ff3b0df1cafc8fac988cabb8c3d416220e
yrkim98 <yrkim98@uw.edu>
2021-04-16 15:45:17 -0700

    moving button creationg to workflow_thumbnails.py

--- napari_aicssegmentation/widgets/workflow_thumbnails.py
+++ napari_aicssegmentation/widgets/workflow_thumbnails.py
@@ -1,6 +1,18 @@
-from qtpy.QtGui import QPixmap
-from qtpy.QtWidgets import QFrame, QHBoxLayout, QLabel, QVBoxLayout, QWidget
-
+from napari.layers.base.base import Layer
+from PyQt5.QtWidgets import (
+    QComboBox,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+    QHBoxLayout,
+    QLayout,
+)
+from PyQt5.QtGui import QIcon, QStandardItem, QStandardItemModel, QPixmap, QImage
+from PyQt5 import QtCore
+from PyQt5.QtCore import QSize
+import numpy as np
+import cv2
 class WorkflowThumbnails(QWidget):
 
     """
@@ -9,17 +21,43 @@ class WorkflowThumbnails(QWidget):
     Params:
     """
 
-    def __init__(self):
+    def __init__(self, workflow_defs):
         super().__init__()
         self.buttons = list()
 
         layout = QVBoxLayout()
         self.enabled = False
         self.setLayout(layout)
+        self.add_buttons(workflow_defs)
 
 
-    def add_buttons(self, button):
-        self.layout().addWidget(button)
+    def add_buttons(self, workflows):
+        for workflow in workflows:
+            if not isinstance(workflow, str):
+                # Some images are RGBA and others are Grayscale
+                # TODO?: convert all images to grayscale?
+                pre, post = workflow.thumbnail_pre, workflow.thumbnail_post
+                # If RGBA convert to grayscale
+                if len(workflow.thumbnail_pre.shape) > 2:
+                    # cv2 expects color channel dim to be last index
+                    pre = cv2.cvtColor(np.moveaxis(workflow.thumbnail_pre, 0, -1), cv2.COLOR_RGBA2GRAY)
+                if len(workflow.thumbnail_post.shape) > 2:
+                    # cv2 expects color channel dim to be last index
+                    post = cv2.cvtColor(np.moveaxis(workflow.thumbnail_post, 0, -1), cv2.COLOR_RGBA2GRAY)
+                # Stitch Image
+                image_stitched = np.hstack([pre, post])
+                button = QPushButton("")
+                # Get np image into QPixmap
+                image = QPixmap(
+                    QImage(image_stitched.data, image_stitched.shape[1], image_stitched.shape[0], QImage.Format_Indexed8))
+                button.setIcon(QIcon(image))
+                button.setIconSize(QSize(PAGE_CONTENT_WIDTH - 40, 200))
+                button.setFixedSize(PAGE_CONTENT_WIDTH, 200)
+
+                button.setEnabled(False)
+
+                self.workflow_buttons.add_buttons(button)
+            self.layout().addWidget(button)
 
     def enable_buttons(self):
         if not self.enabled:
