2a30b575b555aa74c3427994b6b66603fe8e0164
Sylvain Slaton <sylvain.slaton@.alleninstitute.org>
2021-04-26 14:22:37 -0700

    simplify workflow thumbnails widget

--- napari_aicssegmentation/_tests/view/workflow_select_view_test.py
+++ napari_aicssegmentation/_tests/view/workflow_select_view_test.py
@@ -36,7 +36,7 @@ class TestWorkflowSelectView:
 
     def test_load(self):
         # Assert
-        assert self._view.combo_layers.count() == 4  # 4 because of header
+        assert self._view._combo_layers.count() == 4  # 4 because of header
 
     @pytest.mark.parametrize("layers", [None, list()])
     def test_update_layers_without_layers(self, layers):
@@ -44,8 +44,8 @@ class TestWorkflowSelectView:
         self._view.update_layers(layers)
 
         # Assert
-        assert self._view.load_image_warning.isVisibleTo(self._view)
-        assert not self._view.combo_layers.isEnabled()
+        assert self._view._load_image_warning.isVisibleTo(self._view)
+        assert not self._view._combo_layers.isEnabled()
 
     def test_update_layers_with_layers(self):
         layers = ["Layer 1", "Layer 2", "Layer 3"]
@@ -54,17 +54,17 @@ class TestWorkflowSelectView:
         self._view.update_layers(layers, MockLayer("Layer 2"))
 
         # Assert
-        assert self._view.combo_layers.count() == 4  # 4 because of header
-        assert self._view.combo_layers.currentText() == "Layer 2"
-        assert not self._view.load_image_warning.isVisibleTo(self._view)
-        assert self._view.combo_layers.isEnabled()
+        assert self._view._combo_layers.count() == 4  # 4 because of header
+        assert self._view._combo_layers.currentText() == "Layer 2"
+        assert not self._view._load_image_warning.isVisibleTo(self._view)
+        assert self._view._combo_layers.isEnabled()
 
     @pytest.mark.parametrize("channels", [None, list()])
     def test_update_channels_without_channels(self, channels):
         # Act
         self._view.update_channels(channels)
         # Assert
-        assert not self._view.combo_channels.isEnabled()
+        assert not self._view._combo_channels.isEnabled()
 
     def test_update_channels_with_channels(self):
         selected_channel = Channel(2)
@@ -74,27 +74,27 @@ class TestWorkflowSelectView:
         self._view.update_channels(channels, selected_channel)
 
         # Assert
-        assert self._view.combo_channels.count() == 4  # 4 because of header
-        assert self._view.combo_channels.currentData(QtCore.Qt.UserRole) == selected_channel
-        assert self._view.combo_channels.isEnabled()
+        assert self._view._combo_channels.count() == 4  # 4 because of header
+        assert self._view._combo_channels.currentData(QtCore.Qt.UserRole) == selected_channel
+        assert self._view._combo_channels.isEnabled()
 
     def test_combo_layers_activated_select(self):
         # Arrange
-        self._view.combo_layers.addItems(["Layer 1", "Layer 2", "Layer 3"])
+        self._view._combo_layers.addItems(["Layer 1", "Layer 2", "Layer 3"])
 
         # Act
-        self._view.combo_layers.activated.emit(1)
+        self._view._combo_layers.activated.emit(1)
 
         # Assert
         self._mock_controller.select_layer.assert_called_with("Layer 1")
 
     def test_combo_layers_activated_unselect(self):
         # Arrange
-        self._view.combo_layers.addItems(["Layer 1", "Layer 2", "Layer 3"])
+        self._view._combo_layers.addItems(["Layer 1", "Layer 2", "Layer 3"])
 
         # Act
-        self._view.combo_layers.activated.emit(1)
-        self._view.combo_layers.activated.emit(0)
+        self._view._combo_layers.activated.emit(1)
+        self._view._combo_layers.activated.emit(0)
 
         # Assert
         self._mock_controller.unselect_layer.assert_called()
@@ -106,7 +106,7 @@ class TestWorkflowSelectView:
         self._view.update_channels(channels)
 
         # Act
-        self._view.combo_channels.activated.emit(1)
+        self._view._combo_channels.activated.emit(1)
 
         # Assert
         self._mock_controller.select_channel.assert_called_with(expected_channel)
@@ -117,8 +117,8 @@ class TestWorkflowSelectView:
         self._view.update_channels(channels)
 
         # Act
-        self._view.combo_channels.activated.emit(1)
-        self._view.combo_channels.activated.emit(0)
+        self._view._combo_channels.activated.emit(1)
+        self._view._combo_channels.activated.emit(0)
 
         # Assert
         self._mock_controller.unselect_channel.assert_called()
