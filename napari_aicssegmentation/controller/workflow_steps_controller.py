b215aff6d4b5bf64fe467ea35f2a6d617c4de828
Na Hyung Choi <schoinh@gmail.com>
2021-04-26 17:18:17 -0700

    Add comments to run_all

--- napari_aicssegmentation/controller/workflow_steps_controller.py
+++ napari_aicssegmentation/controller/workflow_steps_controller.py
@@ -1,3 +1,5 @@
+from typing import Dict, List
+
 from napari_aicssegmentation.model.segmenter_model import SegmenterModel
 from aicssegmentation.workflow import WorkflowEngine
 from napari_aicssegmentation.util.debug_utils import debug_class
@@ -31,19 +33,24 @@ class WorkflowStepsController(Controller, IWorkflowStepsController):
         self.model.reset()
         self.router.workflow_selection()
 
-    def run_all(self, parameters):
-        """ parameters is list of dictionaries of parameters, one dictionary for each step"""
-        workflow = self.model.active_workflow
-        workflow.reset()
+    def run_all(self, parameter_inputs: List[Dict[str, List]]):
+        """ 
+        parameter_inputs is a list of dictionaries, one dictionary (or None if no parameters) 
+        for each step. Each dictionary has the same shape as a WorkflowStep.parameter_defaults
+        dictionary, but with the parameter values obtained from the UI instead of default values.
+        """
 
+        workflow = self.model.active_workflow
+
+        # This is equivalent to running workflow.execute_all(), but with non-default params
+        workflow.reset()
         step = 0
         while not workflow.is_done():
-            workflow.execute_next(parameters[step])
+            workflow.execute_next(parameter_inputs[step])
             step += 1
-
         result = workflow.get_most_recent_result()
 
-        self.viewer.add_image(result, name=f"Result for workflow {self.model.active_workflow.name}")
+        self.viewer.add_image(result, name=f"Result for workflow {self.model.active_workflow.workflow_definition.name}")
 
         # hide all layers except most recent layer
         for layer in self.viewer.layers[:-1]:
