852bc6e7402124881464bf75fb3a1562451ae994
Sylvain Slaton <sylvain.slaton@.alleninstitute.org>
2021-04-29 11:13:26 -0700

    tests for workflow_steps_view

--- napari_aicssegmentation/_tests/view/workflow_steps_view_test.py
+++ napari_aicssegmentation/_tests/view/workflow_steps_view_test.py
@@ -9,21 +9,20 @@ from aicssegmentation.workflow import WorkflowEngine
 class TestWorkflowStepsView:
     def setup_method(self):
         model = SegmenterModel()
-        # TODO mock this?
         model.active_workflow = WorkflowEngine().get_executable_workflow("sec61b", np.ones((1, 1, 1)))
         self._mock_controller: MagicMock = create_autospec(IWorkflowStepsController)
         self._view = WorkflowStepsView(self._mock_controller)
         self._view.load(model)
 
-    # TODO: Add real tests once we make the UI functional
-    def test_load_model(self):
-        pass
+    def test_show_workflow_diagram(self):
+        assert not self._view.window_workflow_diagram.isVisible()
+        self._view.btn_workflow_info.clicked.emit(False)
+        assert self._view.window_workflow_diagram.isVisible()
 
-    def test_add_workflow_title(self):
-        pass
+    def test_run_all(self):
+        self._view.btn_run_all.clicked.emit(False)
+        self._mock_controller.run_all.assert_called_once()
 
-    def test_add_progress_bar(self):
-        pass
-
-    def test_add_workflow_steps(self):
-        pass
+    def test_close_workflow_keep_layers(self):
+        self._view.btn_close_keep.clicked.emit(False)
+        self._mock_controller.close_workflow.assert_called_once()
