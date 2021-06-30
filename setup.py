d6914d9c523131c9e71a2cb8412371ac0fbe9e0e
Sylvain Slaton <Aerendel@users.noreply.github.com>
2021-06-30 16:42:28 -0700

    Release backmerge (#114)
    * Create publish_to_pypi.yml
    
    * change pyproject
    
    * try original publish.yml
    
    * test
    
    * change user token for pypi
    
    * change scm to fix versioning error
    
    * release to pypi when we merge into release branch
    
    * changing author and license info
    
    * change to release on push
    
    * Bump version: 0.0.0 → 0.0.1
    
    * Bump version: 0.0.1 → 1.0.0
    
    * cleanup
    
    * cleanup
    
    Co-authored-by: Brian Kim <43979182+yrkim98@users.noreply.github.com>
    Co-authored-by: yrkim98 <yrkim98@uw.edu>

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ use_scm = {"write_to": "napari_allencell_segmenter/_version.py"}
 
 setup(
     name="napari-allencell-segmenter",
-    author="Allen Institute for Cell Science",    
+    author="Allen Institute for Cell Science",
     license="BSD-3",
     url="https://github.com/AllenCell/napari-allencell-segmenter",
     description="A plugin that enables 3D image segmentation provided by Allen Institute for Cell Science",
@@ -118,6 +118,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.1",
+    version="1.0.0",
     zip_safe=False,
 )
