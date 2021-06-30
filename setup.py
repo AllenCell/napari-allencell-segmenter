5532d41cd21662ec5a4c6f243c237ad1d360a19a
Sylvain Slaton <Aerendel@users.noreply.github.com>
2021-06-30 10:54:49 -0700

    Feature/release v1 (#112)
    * Create publish_to_pypi.yml
    
    * change pyproject
    
    * try original publish.yml
    
    * test
    
    * change user token for pypi
    
    * change scm to fix versioning error
    
    * release to pypi when we merge into release branch
    
    * changing author and license info
    
    * change to release on push
    
    * Bump version: 0.0.0 → 0.0.1
    
    * metadata / doc updates for v1 release
    
    * fix descriptions
    
    Co-authored-by: Brian Kim <43979182+yrkim98@users.noreply.github.com>
    Co-authored-by: yrkim98 <yrkim98@uw.edu>

--- setup.py
+++ setup.py
@@ -58,8 +58,7 @@ dev_requirements = [
     "wheel>=0.33.1",
 ]
 
-setup_requirements = [
-    "setuptools_scm",
+setup_requirements = [    
     "pytest-runner",
 ]
 
@@ -81,15 +80,14 @@ use_scm = {"write_to": "napari_allencell_segmenter/_version.py"}
 
 setup(
     name="napari-allencell-segmenter",
-    author="Jamie Sherman",
-    author_email="jamies@alleninstitute.org",
+    author="Allen Institute for Cell Science",    
     license="BSD-3",
     url="https://github.com/AllenCell/napari-allencell-segmenter",
-    description="A plugin that enables image segmentation provided by AICS",
+    description="A plugin that enables 3D image segmentation provided by Allen Institute for Cell Science",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=requirements,
     use_scm_version=use_scm,
     setup_requires=setup_requirements,
@@ -98,10 +96,13 @@ setup(
     extras_require=extra_requirements,
     include_package_data=True,
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
         "Framework :: napari",
-        "Topic :: Software Development :: Testing",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -117,6 +118,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.0",
+    version="0.0.1",
     zip_safe=False,
 )
