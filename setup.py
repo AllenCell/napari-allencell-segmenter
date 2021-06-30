cebc12fdbcfb8ffb80fe278d4a6e0a791b946d7e
Sylvain Slaton <sylvain.slaton@alleninstitute.org>
2021-06-30 16:57:21 -0700

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.0",
+    version="1.0.1",
     zip_safe=False,
 )
