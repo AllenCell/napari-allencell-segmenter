4f5bcbc9700b4e0dcf6b805821186ed7c1814b56
yrkim98 <yrkim98@uw.edu>
2021-08-12 16:39:53 -0700

    this branch is a WIP

--- napari_allencell_segmenter/core/layer_reader.py
+++ napari_allencell_segmenter/core/layer_reader.py
@@ -37,7 +37,7 @@ class LayerReader:
         return self._get_channels_default(layer)
 
     def _get_channels_default(self, layer: Layer) -> List[Channel]:
-        img = AICSImage(layer.data)  # gives us a 6D image
+        img = AICSImage(layer.data)  # gives us a 6D image#
 
         # we're expecting either STCZYX or STZCYX but we don't know for sure
         # Attempt to guess based on array length. Channels array should be shorter in general.'
