be737ab3f9948e1dcc7bc94c7c54b62dbc270fc8
yrkim98 <yrkim98@uw.edu>
2021-08-18 10:18:49 -0700

    adding pin for known working version of aicsimageio

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requirements = [
     "numpy",
     "aicssegmentation >= 0.3.0",
     "magicgui >= 0.2.9",
-    "aicsimageio>=3.3.6",
+    "aicsimageio==4.0.5",
     "opencv-python-headless>=4.5.1",
 ]
 
