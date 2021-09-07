1f43ac1f82b834c9139fdf759bf8652331758edb
yrkim98 <yrkim98@uw.edu>
2021-09-07 15:47:27 -0700

    Bump version: 1.0.3 → 1.1.0

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="1.0.3",
+    version="1.1.0",
     zip_safe=False,
 )
