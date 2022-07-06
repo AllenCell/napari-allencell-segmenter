e16861d5ca3f00b9a3b2aa0ccb15ba685c018cc7
Brian Kim <brian.kim@alleninstitute.org>
2022-07-06 11:36:35 -0700

    Bump version: 2.0.0-dev0 → 2.1.0-dev0

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="2.0.0-dev0",
+    version="2.1.0-dev0",
     zip_safe=False,
 )
