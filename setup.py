e2dffade9412d917b19a237444611e1de0e43329
Brian Kim <brian.kim@alleninstitute.org>
2022-08-22 13:25:14 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="2.1.0-dev0",
+    version="2.1.0",
     zip_safe=False,
 )
