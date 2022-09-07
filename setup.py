101ed92d7c234f5fd7ed4dbb035976f86bdfb96d
yrkim98 <yrkim98@uw.edu>
2022-09-07 09:32:43 -0700

    Bump version: 2.1.2-dev0 → 2.1.2

--- setup.py
+++ setup.py
@@ -119,6 +119,6 @@ setup(
     },
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="2.1.2-dev0",
+    version="2.1.2",
     zip_safe=False,
 )
