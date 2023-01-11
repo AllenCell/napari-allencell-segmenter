fbd84f29457edc9b82c0751e0ec629b10d678e3f
Brian Kim <brian.kim@alleninstitute.org>
2023-01-11 10:58:30 -0800

    Bump version: 2.1.7 → 2.1.8

--- napari_allencell_segmenter/__init__.py
+++ napari_allencell_segmenter/__init__.py
@@ -7,4 +7,4 @@ from ._dock_widget import napari_experimental_provide_dock_widget  # noqa: F401
 
 # Do not edit this string manually, always use bumpversion
 # Details in CONTRIBUTING.md
-__version__ = "2.1.7"
+__version__ = "2.1.8"
