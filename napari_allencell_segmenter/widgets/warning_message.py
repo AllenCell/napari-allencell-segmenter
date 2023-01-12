2950257af928de442df27a6d96e84120e6a5fabd
Brian Kim <brian.kim@alleninstitute.org>
2023-01-12 09:38:00 -0800

    sanitize param sweep window user inputs

--- napari_allencell_segmenter/widgets/warning_message.py
+++ napari_allencell_segmenter/widgets/warning_message.py
@@ -3,6 +3,7 @@ from qtpy.QtGui import QPixmap
 from napari_allencell_segmenter.util.directories import Directories
 
 
+
 class WarningMessage(QWidget):
     """
     Warning message Widget with a yellow warning sign icon on the left.
