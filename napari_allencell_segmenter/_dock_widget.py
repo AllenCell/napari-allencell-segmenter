2d415ff36f67e7f7e2f306b2d3c6c7db4fd84b61
Brian Kim <brian.kim@alleninstitute.org>
2023-03-14 14:48:57 -0700

    working rough draft

--- napari_allencell_segmenter/_dock_widget.py
+++ napari_allencell_segmenter/_dock_widget.py
@@ -12,24 +12,26 @@ of the plugin window and the title displayed in the app menu dropdown.
 
 
 class WorkflowEditorWidget(QWidget):  # pragma: no-cover
-    def __init__(self, napari_viewer: napari.Viewer):
+    def __init__(self, viewer: 'napari.viewer.Viewer'):
         super().__init__()
+        self._viewer = viewer
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
         self.setLayout(QVBoxLayout())
         self.layout().setContentsMargins(0, 0, 0, 0)
-        self._application = Application(napari_viewer, self.layout())
+        self._application = Application(viewer, self.layout())
         self._application.router.workflow_selection()  # Initialize first screen
 
 
 class BatchProcessingWidget(QWidget):
-    def __init__(self, napari_viewer: napari.Viewer):
+    def __init__(self, viewer: 'napari.viewer.Viewer'):
         super().__init__()
+        self._viewer = viewer
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
         self.setLayout(QVBoxLayout())
-        self._application = Application(napari_viewer, self.layout())
+        self._application = Application(viewer, self.layout())
         self._application.router.batch_processing()  # Initialize first screen
 
 
-@napari_hook_implementation
-def napari_experimental_provide_dock_widget():  # pragma: no-cover
-    return [(WorkflowEditorWidget, {"name": "Workflow editor"}), (BatchProcessingWidget, {"name": "Batch processing"})]
+# @napari_hook_implementation
+# def napari_experimental_provide_dock_widget():  # pragma: no-cover
+#     return [(WorkflowEditorWidget, {"name": "Workflow editor"}), (BatchProcessingWidget, {"name": "Batch processing"})]
