a816c212e2cad8d3555c2dda12b5ef2d9c1cb666
brian.kim <yrkim98@uw.edu>
2025-10-10 07:54:13 -0700

    itk pins

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ requirements = [
     "opencv-python-headless>=4.5.1",
     "importlib-metadata==4.11.4",
     "npe2",
+    "itk-core==5.2.0.post2",
+    "itkwidgets==0.32.0",
 ]
 
 test_requirements = [
